2fd3be4ba82c265995b695c601dd8ff775c86d65
soriiieee <sorimachi.y.ab@gmail.com>
2020-11-26 09:57:24 +0900

    init

---
+++ py/model_covid19v2.py
@@ -0,0 +1,41 @@
+# https://api.rakuten.net/api-sports/api/api-football?endpoint=apiendpoint_33b2c650-e8fb-4ac6-aa3c-715d2bb5032f
+# reference
+
+import numpy as np
+import sys, os, re, glob
+import re
+# sys.path.append('/Users/soriiieee/.local/lib/python3.7/site-packages')
+import pandas as pd
+import matplotlib.pyplot as plt
+import subprocess
+
+import requests
+import json
+import time
+
+url = "https://api-football-v1.p.rapidapi.com/v2/predictions/157462"
+url = "https://api-football-v1.p.rapidapi.com/v2/countries"
+
+headers = {
+  "x-rapidapi-host": open("../env/api_host.env").read(),
+  "x-rapidapi-key": open("../env/api_football_key.env").read()
+}
+# print(headers)
+# sys.exit()
+# sys.exit()
+
+def load_json(json_path):
+  with open(json_path, "r") as json_file:
+    json_data = json.load(json_file)
+  return json_data
+
+def save_json(json_path):
+  with open(json_path, "w") as json_file:
+    json_data = json.load(json_file)
+  return json_data
+
+if __name__ == "__main__":
+  print("starts...")
+  
+
+
